34f6a3f1f4c13827d1a3ebd537b9da0886180773
ananthks <anan.busyguy007@gmail.com>
2018-11-04 22:19:06 +0100

    added fav icon

--- common/src/main/java/com/learn_french/common/fulldialog/activity/CardFlipActivity.java
+++ common/src/main/java/com/learn_french/common/fulldialog/activity/CardFlipActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.animation.AccelerateDecelerateInterpolator;
--- common/src/main/java/com/learn_french/common/fulldialog/activity/CardOverviewActivity.java
+++ common/src/main/java/com/learn_french/common/fulldialog/activity/CardOverviewActivity.java
@@ -3,19 +3,24 @@ package com.learn_french.common.fulldialog.activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.learn_french.common.R;
+import com.learn_french.common.fulldialog.contracts.FullScreenDialogFragment;
 import com.learn_french.common.fulldialog.utils.Utils;
 
-public class CardOverviewActivity extends AppCompatActivity implements View.OnClickListener {
+public class CardOverviewActivity extends AppCompatActivity implements View.OnClickListener , FullScreenDialogFragment.OnConfirmListener,
+        FullScreenDialogFragment.OnDiscardListener,
+        FullScreenDialogFragment.OnDiscardFromExtraActionListener{
 
     public static String EXTRA_SELECTED_LEVEL = "EXTRA_SELECTED_LEVEL";
     private String selectedLevel;
@@ -23,6 +28,12 @@ public class CardOverviewActivity extends AppCompatActivity implements View.OnCl
     View outerView;
     LinearLayout mLesson;
     LinearLayout mQuiz;
+
+    FullScreenDialogFragment dialogFragment;
+
+    final String dialogTag = "dialog";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +69,46 @@ public class CardOverviewActivity extends AppCompatActivity implements View.OnCl
         return true;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.extra_items, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+       /* //noinspection SimplifiableIfStatement
+        if (id == R.id.action_favorite) {
+            dialogFragment =
+                    (FullScreenDialogFragment) getSupportFragmentManager().findFragmentByTag(dialogTag);
+            if (dialogFragment != null) {
+                dialogFragment.setOnConfirmListener(CardOverviewActivity.this);
+                dialogFragment.setOnDiscardListener(CardOverviewActivity.this);
+                dialogFragment.setOnDiscardFromExtraActionListener(CardOverviewActivity.this);
+            }
+            final Bundle args = new Bundle();
+
+            dialogFragment = new FullScreenDialogFragment.Builder(CardOverviewActivity.this)
+                    .setTitle(com.learn_french.common.R.string.Title)
+                    .setConfirmButton(null)
+                    .setOnConfirmListener(CardOverviewActivity.this)
+                    .setOnDiscardListener(CardOverviewActivity.this)
+                    .setContent(AutoPlayFragment.class, args)
+                    .setExtraActions(com.learn_french.common.R.menu.extra_items)
+                    .setOnDiscardFromActionListener(CardOverviewActivity.this)
+                    .build();
+
+            dialogFragment.show(getSupportFragmentManager(), dialogTag);
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
     public void presentActivity(View view, boolean isLesson) {
         ActivityOptionsCompat options = ActivityOptionsCompat.
                 makeSceneTransitionAnimation(this, view, "transition");
@@ -80,5 +131,20 @@ public class CardOverviewActivity extends AppCompatActivity implements View.OnCl
         }else if(view.getId()==R.id.exam){
             presentActivity(view, false);
         }
+    }
+
+    @Override
+    public void onConfirm(@Nullable Bundle result) {
+
+    }
+
+    @Override
+    public void onDiscard() {
+
+    }
+
+    @Override
+    public void onDiscardFromExtraAction(int actionId, @Nullable Bundle result) {
+
     }
 }
